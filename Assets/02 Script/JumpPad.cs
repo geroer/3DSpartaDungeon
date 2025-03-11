ce5b3ab9c81f45cd6401b1cd4da3b1f7f377177b
geroer <110544587+geroer@users.noreply.github.com>
2025-03-11 13:38:03 +0900

    [Feat] 움직이는 발판

--- Assets/02 Script/JumpPad.cs
+++ Assets/02 Script/JumpPad.cs
@@ -1,14 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
     public float jumpForce;
 
+    [Header("Movement")]
+    public Transform startPoint;
+    public Transform endPoint;
+    public float moveSpeed = 0.2f;
+    public float waitTime = 2f;
+    private bool isMove = true;
+
+    private void Start()
+    {
+        StartCoroutine(moveCoroutine());
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             collision.rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator moveCoroutine()
+    {
+        while (true)
+        {
+            Vector3 pos = isMove ? endPoint.position : startPoint.position;
+
+            while (Vector3.Distance(transform.position, pos) > 0.1f)
+            {
+                transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * moveSpeed);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(waitTime);
+            isMove = !isMove;
         }
     }
 }
